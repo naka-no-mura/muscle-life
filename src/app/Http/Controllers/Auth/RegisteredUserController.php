ca2b9aaf36247b9c2241a2c4b81641efdc475dcb
naka-no-mura <nakanomura.ryu@gmail.com>
2023-08-21 18:00:19 +0900

    welcome画面から認証コードの送信まで

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Register\PreRegisterEmailVerificationRequest;
+use App\Mail\PreRegisterEmailVerification;
+use App\Models\EmailVerification;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -10,12 +13,40 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
+    /**
+     * 認証コードをメールで送信
+     *
+     * @param PreRegisterEmailVerificationRequest $request
+     * @return Response
+     */
+    public function preRegisterEmailVerification(PreRegisterEmailVerificationRequest $request): Response
+    {
+        // 6桁の認証コード生成
+        $auth_code = null;
+
+        while ($auth_code === null) {
+            $tmp_auth_code = (int)str_pad(mt_rand(0, 999999), 6, 0, STR_PAD_LEFT);
+            $count = EmailVerification::where('auth_code', $tmp_auth_code)->count();
+
+            // ダブっていなければ認証コードとして採用
+            if ($count == 0) {
+                $auth_code = $tmp_auth_code;
+            }
+        }
+
+        // 認証コードをメール送信
+        Mail::to($request->email)->send(new PreRegisterEmailVerification($auth_code));
+
+        return Inertia::render('Auth/AuthCode');
+    }
+
     /**
      * Display the registration view.
      */
--- src/routes/auth.php
+++ src/routes/auth.php
@@ -12,6 +12,9 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    Route::post('send-auth-code', [RegisteredUserController::class, 'preRegisterEmailVerification'])
+                ->name('send.auth.code');
+
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
